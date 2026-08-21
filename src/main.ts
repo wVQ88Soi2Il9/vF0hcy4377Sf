c7ea98b4a7672ba321b007369f9e3c473d0de4db
wVQ88Soi2Il9 <7gulj74xv10d@gmail.com>
2026-08-22 04:12:25 +0800

    resize defualt map

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ for (const p of load_all_packs())
 load_all_device_classes(registry);
 
 // 2. 建立地圖（3 維空間: 20×20×8，全偶數網格錨點）
-const map = create_map([20, 20, 8]);
+const map = create_map([64, 64, 8]);
 
 // 3. 在 loader 呼叫 init_pack() 之前，註冊 map 和 registry
 set_map(map);
