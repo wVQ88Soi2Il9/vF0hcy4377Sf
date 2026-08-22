48da798ee08d3ce89df3f29f12676cc55b0c79d8
wVQ88Soi2Il9 <7gulj74xv10d@gmail.com>
2026-08-22 20:34:06 +0800

    build: add singlefile plugin for offline standalone bundle distribution

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue()],
+  plugins: [vue(), viteSingleFile()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
