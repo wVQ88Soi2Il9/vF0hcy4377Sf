ae79cb6650c74cb184720c7589b048b834bb62e0
wVQ88Soi2Il9 <7gulj74xv10d@gmail.com>
2026-09-03 21:02:05 +0800

    小修格式

--- src/packs/empty_pack/init.ts
+++ src/packs/empty_pack/init.ts
@@ -2,9 +2,12 @@ import { register_pack, type pack_registry } from '@/core';
 
 export function global_init(registry: pack_registry): void
 {
-    register_pack(registry, {
+    register_pack
+    (registry, 
+        {
         pack_id: 'empty_pack'
-    });
+        }
+    );
 }
 
 export function local_init(): void
