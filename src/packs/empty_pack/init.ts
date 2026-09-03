cf4b78aebc62b9b491f3c82580f10dfd32c387e0
wVQ88Soi2Il9 <7gulj74xv10d@gmail.com>
2026-09-03 21:06:17 +0800

    feat(empty_pack): declare hook foo in global_init and inject callback bar in local_init

--- src/packs/empty_pack/init.ts
+++ src/packs/empty_pack/init.ts
@@ -1,16 +1,21 @@
-import { register_pack, type pack_registry } from '@/core';
+import { register_pack, inject_world_hook, type pack_registry, type pure_world } from '@/core';
+
+export function bar(): void
+{
+
+}
 
 export function global_init(registry: pack_registry): void
 {
-    register_pack
-    (registry, 
-        {
-        pack_id: 'empty_pack'
-        }
-    );
+    register_pack(registry, {
+        pack_id: 'empty_pack',
+        hooks: new Map([
+            ['foo', []]
+        ])
+    });
 }
 
-export function local_init(): void
+export function local_init(target_world: pure_world): void
 {
-
+    inject_world_hook(target_world, { namespace: 'empty_pack', id: 'foo' }, bar);
 }
